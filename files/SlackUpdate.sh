d5163c5962c89a6e0818748aad795cfb393af9b2
cwagnell <110456062+cwagnell@users.noreply.github.com>
2024-12-08 15:20:49 +0800

    Update SlackUpdate.sh

--- files/SlackUpdate.sh
+++ files/SlackUpdate.sh
@@ -3,6 +3,6 @@ pattern="]: [^<].*[^.] joined the game"
 while IFS= read line; do 
 	newLine=$(grep "$pattern" <<< "$line")
 	if [ "$newLine" != "" ]
-		then curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"MC Server: $newLine\"}" _URL_
+		then curl --silent --output /dev/null -X POST -H 'Content-type: application/json' --data "{\"text\":\"MC Server: $newLine\"}" _URL_
 	fi 
 done
