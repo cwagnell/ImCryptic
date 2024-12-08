658685ac60ed29a110c32243915c89cef4b76b17
cwagnell <110456062+cwagnell@users.noreply.github.com>
2024-12-08 15:51:28 +0800

    Update SlackUpdate.sh

--- files/SlackUpdate.sh
+++ files/SlackUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pattern="]: [^<].*[^.] joined the game"
-tail -f /home/cam/logs/latest.log \
+tail -f /home/container/logs/latest.log \
         | grep --line-buffered "$pattern" \
         | while read -r line; do
-                curl --silent --output /dev/null -X POST -H 'Content-type: application/json' --data "{\"text\":\"MC Server: $line\"}" https://hooks.slack.com/services/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
+                curl --silent --output /dev/null -X POST -H 'Content-type: application/json' --data "{\"text\":\"MC Server>
         done
